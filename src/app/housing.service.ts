e93d203fbdd5c0b28a705bae25cab52b0fa7b3c2
Eduardo R. Duraes <eduardoduraes.bsi@gmail.com>
2024-04-24 11:08:06 -0300

    Lesson 09: Angular services

---
+++ src/app/housing.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HousingLocation} from "./housing-location/housing-location";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HousingService {
+
+  constructor() { }
+
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  }
+}
